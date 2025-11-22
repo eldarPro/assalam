5ac3526539d9f4ccd89fc1ea3f10aae0ad4947fc
Eldar <eldar@telega.in>
2025-11-22 15:40:49 +0300

    user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
+
+  GENDERS = %w[male female]
+
   has_many :sessions, dependent: :destroy
 
-  normalizes :email_address, with: ->(e) { e.strip.downcase }
+  validates :fio, presence: true
+  validates :phone, format: /\A\+?\d{10,15}\z/
+  validates :gender, inclusion: { in: GENDERS }, allow_nil: true
 end
--- db/migrate/20251119185722_create_users.rb
+++ db/migrate/20251119185722_create_users.rb
@@ -1,11 +1,18 @@
 class CreateUsers < ActiveRecord::Migration[8.1]
   def change
+    execute <<-SQL
+      CREATE TYPE gender AS ENUM ('male', 'female');
+    SQL
+
     create_table :users do |t|
-      t.string :email_address, null: false
+      t.string :fio
+      t.string :email
+      t.column :gender, :gender, null: true
+      t.string :phone, null: false
       t.string :password_digest, null: false
 
       t.timestamps
     end
-    add_index :users, :email_address, unique: true
+    add_index :users, :phone, unique: true
   end
 end
